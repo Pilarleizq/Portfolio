92038feaab6ac6e42e6387294c151db91de7a879
Pilarleizq <pleivaizq@gmail.com>
2023-06-03 17:14:36 +0200

    layout almost finished

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,21 @@ import '../styles/App.scss';
 import logo from '../images/pleizq.png'
 import photo from '../images/foto.jpeg'
 import house from '../images/casa.png'
-// import foto from '../images/foto.jpeg';
-// import footer from '../images/stay-at-home.png'
+import desarrollalobas from '../images/desarrollalobas.png'
+import numbergame from '../images/numbergame.png'
+import cards from '../images/cards.png'
+import aikido from '../images/aikido.png'
+import harrypotter from '../images/harrypotter.png'
+import fop from '../images/fop.png'
+import cocktails from '../images/cocktails.png'
+import instagram from '../images/instagram.png'
+import linkedin from '../images/linkedin.png'
+import github from '../images/github.png'
+import email from '../images/gmail.png'
+import phone from '../images/llamada-telefonica.png'
 
 //React
 // import { useEffect, useState } from 'react';
-
-//Míos 
-
 
 function App() {
 
@@ -40,27 +47,48 @@ function App() {
       <section className="section_1">
           <article className="section__projects">Proyectos</article>
             <div className="projects_div">
-              <p>Desarrolalobas</p>
-              <p>Adivina el número</p>
-              <p>PowerpuffLopers</p>
-              <p>Friends</p>
-              <p>Harry Potter</p>
-              <p>Full Of Proyects (FOP)</p>
+              <article>
+              <img className="projects_div_each" src={desarrollalobas} alt="proyecto"></img>
+              </article>
+              <article>
+              <img className="projects_div_each" src={numbergame} alt="proyecto"/>
+              </article>
+              <article>
+              <img className="projects_div_each" src={cards} alt="proyecto"/>
+              </article>
+              <article>
+              <img className="projects_div_each" src={aikido} alt="proyecto"/>
+              </article>
+              <article>
+              <img className="projects_div_each" src={harrypotter} alt="proyecto"/>
+              </article>
+              <article>
+              <img className="projects_div_each" src={fop} alt="proyecto"/>
+              </article>
+              <article>
+              <img className="projects_div_each" src={cocktails} alt="proyecto"/>
+              </article>
             </div>
 
       </section>
       <section className="section_2">
           <article className="section__rrss">Redes Sociales</article>
           <div className="rrss_div">
-            <p>Instagram</p>
-            <p>LinkedIn</p>
-            <p>GitHub</p>
+            <img className="rrss_div_each" src={instagram} alt="logo-instagram"/>
+            <img className="rrss_div_each" src={linkedin} alt="logo-linkedin"></img>
+            <img className="rrss_div_each" src={github} alt="logo-github"></img>
           </div>
 
           <article className="section__contact">Contacto</article>
           <div className="contact_div">
-            <p>Email</p>
-            <p>Teléfono</p>
+            <article className="contact_div_article">
+            <img className="contact_icon" src={email} alt="icono-email"/>
+            <p> pleivaizq@gmail.com</p>
+            </article>
+            <article className="contact_div_article">
+            <img className="contact_icon" src={phone} alt="icono-telefono"/> 
+            <p>608361103</p>
+            </article>
           </div>
       </section>
       </main>
